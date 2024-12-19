7e820fe5cf0f9d456e7128d4f77d011601c7a6a2
Alexisdlr1 <alexis@steamhub.com.mx>
2024-12-18 20:56:01 -0700

    nwpr

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -45,7 +45,7 @@ const getGroupedTransactions = async (req, res) => {
 // Controlador para crear una transacción
 const createTransaction = async (req, res) => {
     try {
-        const { userId, amount, status } = req.body;
+        const { userId, amount, date } = req.body;
 
         if (!userId || !amount) {
             return res.status(400).json({ error: "userId y amount son requeridos." });
@@ -59,7 +59,7 @@ const createTransaction = async (req, res) => {
         const transaction = new Transaction({
             userId,
             amount,
-            status,
+            date,
         });
 
         const savedTransaction = await transaction.save();
