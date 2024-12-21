c9fb1346ec37e47d16b40269a7b53dcedaed494b
Alexisdlr1 <alexis@steamhub.com.mx>
2024-12-21 14:23:26 -0700

    finally

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.json()); // Esta línea es importante
 app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
-  res.status(200).send("Backend and Github connected.");
+  res.status(200).send("Backend and Github connected!!!!!!.");
 });
 
 // Rutas
