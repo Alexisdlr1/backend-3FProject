7c8fd0379196f5aec69b69e05d3a273282c6e5bb
Alexisdlr1 <alexis@steamhub.com.mx>
2024-12-21 13:51:17 -0700

    YESSSSSSS

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.json()); // Esta línea es importante
 app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
-  res.status(200).send("Con esto deberia funcionar?.");
+  res.status(200).send("The backend update on the main branch in production is ready.");
 });
 
 // Rutas
