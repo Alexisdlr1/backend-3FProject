0210dd8c7f10be7a373a29ce0129086faa87a072
Alexisdlr1 <alexis@steamhub.com.mx>
2024-12-21 14:20:43 -0700

    finally

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.json()); // Esta línea es importante
 app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
-  res.status(200).send("Now is working.");
+  res.status(200).send("Last test.");
 });
 
 // Rutas
