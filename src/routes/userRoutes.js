645c3ba01890275b65452973951f28f982f932c4
CesarTun <ctuncetina@gmail.com>
2025-01-28 18:57:16 -0600

    feat: add withdrawal wallet to user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,17 @@
 const express = require("express");
-const { createUser, getAllUsers, loginUser, checkWallet, updateUser, getUserById, getReferersCommissions, resetPassword, getNotificationsBySingleEmail } = require("../controllers/userController");
+const { 
+  createUser,
+  getAllUsers,
+  loginUser,
+  checkWallet,
+  updateUser,
+  getUserById,
+  getReferersCommissions,
+  resetPassword,
+  getNotificationsBySingleEmail,
+  addWithdrawWallet,
+  disableWithdrawWallet
+} = require("../controllers/userController");
 
 const router = express.Router();
 
@@ -11,6 +23,18 @@ router.post("/check-wallet", checkWallet);
 
 // Ruta para crear un usuario
 router.post("/", createUser);
+
+// Cambiar contraseña
+router.post("/resetPassword", resetPassword);
+
+// Obtener notificaciones by email
+router.post("/notifications", getNotificationsBySingleEmail);
+
+// Agregar wallet para retiro
+router.post("/addWallet/:id", addWithdrawWallet);
+
+// Desactivar wallet para retiro
+router.post("/disableWallet/:id", disableWithdrawWallet);
 
 // Ruta para obtener todos los usuarios
 router.get("/", getAllUsers);
@@ -24,10 +48,5 @@ router.get("/referers/:wallet", getReferersCommissions);
 // Ruta para actualizar un campo de un usuario por id
 router.put("/:id", updateUser);
 
-// Cambiar contraseña
-router.post("/resetPassword", resetPassword);
-
-// Obtener notificaciones by email
-router.post("/notifications", getNotificationsBySingleEmail);
 
 module.exports = router;
