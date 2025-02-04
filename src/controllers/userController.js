4d5954a12fc272416a9decae01a7c220fae5affb
CesarTun <ctuncetina@gmail.com>
2025-02-03 21:02:56 -0600

    feat: add withdrawalWallet for getUserById endpoint

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -43,7 +43,7 @@ const getUserById = async (req, res) => {
 
     const userId = req.params.id;
 
-    const user = await User.findById(userId).select("name email isAdmin isActive wallet referrals uplineCommisions name_beneficiary email_beneficiary withdrawalWallets");
+    const user = await User.findById(userId).select("name email isAdmin isActive wallet referrals uplineCommisions name_beneficiary email_beneficiary withdrawalWallet");
 
     if (!user) {
       return res.status(404).json({ message: "Usuario no encontrado." });
